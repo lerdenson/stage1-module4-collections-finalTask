c12bdd69b52b564a06355396741b55d777e224e0
Максим <Maxim.d.morozov@gmail.com>
2023-07-12 20:27:48 +0400

    done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,25 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> projectNames = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> e : projects.entrySet()) {
+            if (e.getValue().contains(developer)) projectNames.add(e.getKey());
+        }
+
+        projectNames.sort(new projectComparator());
+        return projectNames;
     }
+
+    class projectComparator implements Comparator<String> {
+
+        public int compare(String str1, String str2) {
+
+            if (str1.length() == str2.length()) return str2.compareTo(str1);
+            else return str2.length() - str1.length();
+        }
+    }
+
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,28 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> res = new HashMap<>();
+
+        for (Map.Entry<String, Integer> e: sourceMap.entrySet()) {
+            Set<String> keys = new HashSet<>();
+
+            if (res.containsKey(e.getKey().length())) {
+                keys.addAll(res.get(e.getKey().length()));
+                keys.add(e.getKey());
+
+
+                res.replace(e.getKey().length(), keys);
+            }
+            else {
+                System.out.println(e.getKey());
+                keys.add(e.getKey());
+                res.put(e.getKey().length(), keys);
+            }
+        }
+
+        return res;
     }
 }
